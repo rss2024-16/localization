efa62a7d86dbb5a2fca3b9c8baf04035b601e627
TrevorJohst <trejohst@mit.edu>
2024-05-10 02:54:33 +0000

    Fixed bug in motion model and added noiseless evaluation

--- localization/motion_model.py
+++ localization/motion_model.py
@@ -9,6 +9,36 @@ class MotionModel:
         self.k_vy = .005
         self.k_vtheta = np.pi / 60
 
+    def evaluate_noiseless(self, pose, odometry):
+        """
+        Updates a single pose based on odometry data,
+        does not add any noise to the motion.
+
+        args:
+            pose: A 3-vector [x y theta] 
+
+            odometry: A 3-vector [dx dy dtheta]
+
+        returns:
+            pose: an updated position based on the odometry
+        """
+        dx, dy, dtheta = odometry
+
+        # Transformation matrix calculations
+        cos_theta = np.cos(pose[2])
+        sin_theta = np.sin(pose[2])
+
+        # Compute the transformation for dx, dy considering the new orientation
+        dx_prime = cos_theta * dx - sin_theta * dy
+        dy_prime = sin_theta * dx + cos_theta * dy
+
+        # Update particle positions
+        pose[0] += dx_prime
+        pose[1] += dy_prime
+        pose[2] -= dtheta
+
+        return pose
+
     def evaluate(self, particles, odometry, velocity):
         """
         Update the particles to reflect probable
@@ -38,18 +68,17 @@ class MotionModel:
         y_noise = (self.trans_std + self.k_vy * vy) * np.random.randn(n_particles)
         theta_noise = (self.rot_std + self.k_vtheta * vtheta) * np.random.randn(n_particles)
 
-        # Update particle orientations
-        new_thetas = particles[:, 2] + dtheta + theta_noise
-        cos_theta = np.cos(new_thetas)
-        sin_theta = np.sin(new_thetas)
+        # Transformation matrix calculations
+        cos_theta = np.cos(particles[:, 2])
+        sin_theta = np.sin(particles[:, 2])
 
         # Compute the transformation for dx, dy considering the new orientation
-        dx_prime = cos_theta * dx - sin_theta * dy + x_noise
-        dy_prime = sin_theta * dx + cos_theta * dy + y_noise
+        dx_prime = cos_theta * (dx + x_noise) - sin_theta * (dy + y_noise)
+        dy_prime = sin_theta * (dx + x_noise) + cos_theta * (dy + y_noise)
 
         # Update particle positions
         particles[:, 0] += dx_prime
         particles[:, 1] += dy_prime
-        particles[:, 2] = new_thetas  # Update thetas
+        particles[:, 2] = particles[:, 2] - dtheta + theta_noise
 
         return particles
